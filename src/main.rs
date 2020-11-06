49675fe413fb41a8c1b370aef57d9b20f590d721
Denilson <denilson020898@gmail.com>
2020-11-06 23:57:06 +0700

    handling error properly by short circuiting

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,47 @@ use std::io::BufRead;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::File::open("urls.txt")?;
     let buffile = std::io::BufReader::new(file);
-    println!("URL, Status");
+
+    println!("URL,Status");
 
     let client = reqwest::Client::new();
 
-    let mut handles = Vec::new();
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+    let mut count = 0;
 
     for line in buffile.lines() {
         let line = line?;
 
         let client = client.clone();
-        let handle = tokio::spawn(async move {
-            let resp = client.get(&line).send().await.unwrap();
-            println!("{},{}", line, resp.status().as_u16());
+        let tx = tx.clone();
+        tokio::spawn(async move {
+            let msg = client.get(&line).send().await.map(|resp| {
+                println!("{},{}", line, resp.status().as_u16());
+            });
+            tx.send(msg).unwrap();
         });
-        handles.push(handle);
+
+        count += 1;
     }
 
-    for handle in handles {
-        handle.await?;
-    }
+    std::mem::drop(tx);
 
-    Ok(())
+    let mut i = 0;
+    loop {
+        match rx.recv().await {
+            None => {
+                assert_eq!(i, count);
+                break Ok(());
+            }
+            Some(Ok(())) => {
+                assert!(i < count);
+            }
+            Some(Err(e)) => {
+                assert!(i < count);
+                return Err(From::from(e));
+            }
+        }
+        i += 1;
+    }
 }
